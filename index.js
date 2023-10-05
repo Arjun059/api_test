586ec534799d80cd2765a32e59c404fddae2e7c3
Arjun059 <arjunchitara35081@gmail.com>
2023-10-05 22:29:26 +0530

    frist commi t

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express"); 
+const app = express()
+
+
+app.get("/hello", (req, res) => {
+    res.send("hello You")
+})
+
+app.get("/test", (req, res) => {
+    res.send("Testing ")
+})
+
+
+app.listen(8000, () => {
+
+})
