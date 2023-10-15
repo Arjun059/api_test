66d8f54a62c0094be8c3117f7140d64355d43b45
Arjun059 <arjunchitara35081@gmail.com>
2023-10-15 22:17:13 +0530

    ff

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const app = express()
 const testRoutes = require("./routes/test.js")
 app.use("/api/", testRoutes)
 
+app.use("*", (req, res) => {
+
+    let path = req.originalUrl
+    res.send(JSON.stringify(path))
+
+})
+
 app.listen(8000, () => { })
