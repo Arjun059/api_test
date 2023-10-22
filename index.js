388844098f0a12ac9ddf5e233bbc88a9360b7eaf
Arjun059 <80092032+Arjun059@users.noreply.github.com>
2023-10-22 10:52:18 +0530

    Update index.js
    asdf

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 
 const testRoutes = require("./routes/test.js")
 app.use("/api/", testRoutes)
-app.get("/cool", (req, res) => {
+app.get("/api/cool", (req, res) => {
 
    res.send("api cool get ")
   
