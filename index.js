0d773dee5b09d1c878a0fcd850503e192ab8d5f0
Arjun059 <arjunchitara35081@gmail.com>
2024-07-26 11:11:51 +0530

    chagn

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 
 const testRoutes = require('./routes/test.js');
-app.use('/api/', testRoutes);
+app.use('/api/',
+  
+  testRoutes);
 
 app.app('*', (req, res) => {
   res.send('api route get ');
